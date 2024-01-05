13cec46cc7d73cd95857a3ec3e332fcb3353b2b0
d33bs <dave.bunten@cuanschutz.edu>
2024-01-05 14:21:55 -0700

    remove pandas; rename basic tests

---
+++ src/pymaccounter/tests/test_multiply_gc.debug_leak copy.py
@@ -0,0 +1,30 @@
+"""
+Module for testing pure Python garbage collection.
+"""
+import gc
+
+
+# set gc to debug leak mode
+# see here for more info:
+# https://docs.python.org/3/library/gc.html#gc.DEBUG_LEAK
+gc.set_debug(gc.DEBUG_LEAK)
+
+
+# modified example from:
+# https://parsl.readthedocs.io/en/stable/1-parsl-introduction.html#Python-Apps
+def multiply(a, b):
+    return a * b
+
+
+print(multiply(5, 9))
+
+# collect garbage
+# see here for more info:
+# https://docs.python.org/3/library/gc.html#gc.collect
+collected = gc.collect()
+
+# print the unreachable objects
+print(collected)
+
+# print the len of gc garbage to reference unreachable objects
+print(len(gc.garbage))
