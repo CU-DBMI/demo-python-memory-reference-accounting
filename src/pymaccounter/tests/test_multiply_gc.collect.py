7f9bfeeca7e8b679903468bc5b6ebef8305da17d
d33bs <dave.bunten@cuanschutz.edu>
2024-01-10 09:06:39 -0700

    update tests with asserts for garbage

--- src/pymaccounter/tests/test_multiply_gc.collect.py
+++ src/pymaccounter/tests/test_multiply_gc.collect.py
@@ -13,15 +13,18 @@ def multiply(a, b):
     return a * b
 
 
-print(multiply(5, 9))
+print(f"Function output: {multiply(5, 9)}")
 
 # collect garbage
 # see here for more info:
 # https://docs.python.org/3/library/gc.html#gc.collect
-collected = gc.collect()
+COLLECTED = gc.collect()
 
 # print the unreachable objects
-print(collected)
+print(COLLECTED)
+assert COLLECTED == 0
 
 # print the len of gc garbage to reference unreachable objects
-print(len(gc.garbage))
+GC_LEN = len(gc.garbage)
+print(GC_LEN)
+assert GC_LEN == 0
