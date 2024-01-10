df22eb9421d5c2d11b98dfb02534e005732c222a
d33bs <dave.bunten@cuanschutz.edu>
2024-01-10 09:22:43 -0700

    add debug config; add lines for visual clarity

--- src/pymaccounter/pipeline.py
+++ src/pymaccounter/pipeline.py
@@ -5,6 +5,7 @@ Dagger pipelines using the Python SDK for pymaccounter
 # referenced with modifications from:
 # https://docs.dagger.io/sdk/python/628797/get-started
 
+import shutil
 import sys
 from typing import List
 
@@ -13,18 +14,29 @@ import dagger
 
 
 async def pipeline_run_tests(
-    tests_to_run: List[str], test_dir: str = "src/pymaccounter/tests"
+    tests_to_run: List[str],
+    test_dir: str = "src/pymaccounter/tests",
+    debug: bool = False,
 ) -> None:
     """
     Dagger pipeline for running reproducible tests in python.
     """
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+
+    # create dagger conf based on debug arg
+    dagger_conf = dagger.Config(log_output=sys.stderr) if debug else dagger.Config()
+
+    async with dagger.Connection(dagger_conf) as client:
         # get reference to the local project
         dockerfile_dir = client.host().directory(".")
 
         async def run_test(test_to_run: str, test_dir: str):
-            """ """
+            """
+            Run a test in a container via dagger
+            """
             full_test_to_run = f"{test_dir}/{test_to_run}"
+
+            # gather the terminal width for result display purposes
+            terminal_width, _ = shutil.get_terminal_size()
 
             # build a python container based on Dockerfile and run test
             python = (
@@ -42,6 +54,9 @@ async def pipeline_run_tests(
 
             # execute and show the results of the last executed command
             results = await python.stdout()
+
+            # surround the output with terminal-width lines for reading clarity
+            print("_" * terminal_width)
             print(f"Test results for {full_test_to_run}:\n{results}")
 
         # when this block exits, all tasks will be awaited (i.e., executed)
